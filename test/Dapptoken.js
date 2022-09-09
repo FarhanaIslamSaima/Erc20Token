f5407602f2f7fb9b9b67ed2e6b809f4333d03270
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-09-09 20:33:09 +0600

    file updated

--- test/Dapptoken.js
+++ test/Dapptoken.js
@@ -10,6 +10,11 @@
 
 
 
+
+
+
+
+
 const DappToken = artifacts.require("DappToken");
 var tokenInstance;
 
@@ -102,6 +107,46 @@ it('approve tokens for delegated transfer',function(){
 
     }).then(function(allowance){
         assert.equal(allowance,100,'stores the allowance for delegated transfer')
+    })
+})
+
+
+it('handles delegated transfer',function(){
+    return DappToken.deployed().then(function(instance){
+        tokenInstance=instance;
+        fromAccount=accounts[2];
+        toAccount=accounts[3];
+        spendingAccount=accounts[4];
+        return tokenInstance.transfer(fromAccount,100,{from:accounts[0]})
+    }).then(function(reciept){
+        return tokenInstance.approve(spendingAccount,10,{from: fromAccount})
+    }).then(function(reciept){
+        return tokenInstance.transferFrom(fromAccount,toAccount,9999,{from:spendingAccount})
+    }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert')>=0,'cannot transfer larger ammount than balance')
+        return tokenInstance.transferFrom(fromAccount,toAccount,20,{from:spendingAccount})
+    }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert')>=0,'can not transfer larger than approved amound')
+        return tokenInstance.transferFrom.call(fromAccount,toAccount,10,{from:spendingAccount})
+    }).then(function(success){
+        assert.equal(success,true,'return must be true')
+        return tokenInstance.transferFrom(fromAccount,toAccount,10,{from:spendingAccount})
+    }).then(function(reciept){
+        assert.equal(reciept.logs.length,1,'triggers one event');
+        assert.equal(reciept.logs[0].event,'Transfer','should be the Trasnfer event');
+        assert.equal(reciept.logs[0].args._from,fromAccount,'logs the account the tokens transfer from');
+        assert.equal(reciept.logs[0].args._to,toAccount,'logs the account tokens tranferred to');
+        assert.equal(reciept.logs[0].args._value,10,'transfer amount')
+        return tokenInstance.balanceOf(fromAccount)
+
+    }).then(function(balance){
+        assert.equal(balance.toNumber(),90,'90 is correct')
+        return tokenInstance.balanceOf(toAccount)
+    }).then(function(balance){
+        assert.equal(balance.toNumber(),10,'balance should be 10 token')
+        return tokenInstance.allowance(fromAccount,spendingAccount)
+    }).then(function(token){
+        assert.equal(token.toNumber(),0,'token is 0')
     })
 })
 
